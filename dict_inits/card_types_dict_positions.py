f06993843711bb1fd765aaf4a0f1a41179ba0be9
C-C-Coalback <argarfield18@gmail.com>
2025-11-05 18:15:29 +0100

    Move dictionaries to separate files.

---
+++ dict_inits/card_types_dict_positions.py
@@ -0,0 +1,48 @@
+card_types_dictionary_positions = {
+    "Army": {
+        "Text Box": (0, 0),
+        "Art": (0, 0),
+        "Text": (264, 1416),
+        "Cost": (110, 60),
+        "Attack": (95, 1415),
+        "Health": (95, 1685),
+        "Loyalty": (1313, 950),
+        "Expansion Icon": (1350, 1908)
+    },
+    "Support": {
+        "Text Box": (0, 0),
+        "Art": (0, 0),
+        "Text": (64, 1466),
+        "Cost": (130, 60),
+        "Loyalty": (1313, 950),
+        "Expansion Icon": (1358, 1910)
+    },
+    "Attachment": {
+        "Text Box": (0, 0),
+        "Art": (0, 0),
+        "Text": (64, 1466),
+        "Cost": (140, 94),
+        "Loyalty": (1313, 825),
+        "Shield": (0, 350),
+        "Expansion Icon": (1358, 1915)
+    },
+    "Event": {
+        "Text Box": (0, 0),
+        "Art": (0, 0),
+        "Text": (64, 1466),
+        "Cost": (114, 56),
+        "Loyalty": (1313, 950),
+        "Shield": (0, 350),
+        "Expansion Icon": (1358, 1905)
+    },
+    "Warlord": {
+        "Text Box": (0, 0),
+        "Art": (0, 0),
+        "Text": (164, 400),
+        "Attack": (105, 1515),
+        "Health": (105, 1785),
+        "Cards": (600, 1825),
+        "Resources": (780, 1825),
+        "Expansion Icon": (1358, 1910)
+    }
+}
