faa42e05640b9eca660837df714bdd86cd1dd8fb
C-C-Coalback <argarfield18@gmail.com>
2025-11-06 10:51:09 +0100

    Separate tkinter card parameter setting from the image processing.

--- main.py
+++ main.py
@@ -258,103 +258,150 @@ def add_command_icons(command, first_command_src, extra_command_src, command_end
         resulting_img.paste(command_end_img, (current_x_pos_end_command, y_end_command), command_end_img)
 
 
-def process_submitted_card():
+def get_parameters_then_process():
     global card_image
     global panel
     name = name_area.get("1.0", "end-1c")
     card_type = card_type_label.cget("text")
     card_type = card_type.replace("Card Type: ", "")
     text = text_box_area.get("1.0", "end-1c")
+    output_dir = "current_card_info/resulting_image.png"
     if card_type == "Planet":
         cards_value = planet_cards_area.get("1.0", "end-1c")
         resources_value = planet_resources_area.get("1.0", "end-1c")
         icons_grouped = planet_icons_area.get("1.0", "end-1c")
-        resources_src = "card_srcs/Planet/Values/resource_" + resources_value + ".jpg"
-        cards_src = "card_srcs/Planet/Values/card_" + cards_value + ".jpg"
-        text_src = "card_srcs/" + card_type + "/Text/Text.png"
-        if not os.path.exists(text_src):
-            return False
-        card_art_src = "current_card_info/src_img/img.png"
-        expansion_icon_src = "current_card_info/expansion_icon/expansion_icon.png"
-        output_dir = "current_card_info/resulting_image.png"
-        resulting_img = Image.new("RGBA", (1440, 2052))
-        card_art_img = Image.open(card_art_src, 'r').convert("RGBA")
-        card_art_img = card_art_img.resize((1440, 2052))
-        resulting_img.paste(card_art_img, get_position_text(card_type, "Planet", "Art"))
-        text_resize_amount = (1440, 2052)
-        text_img = Image.open(text_src, 'r').convert("RGBA")
-        text_img = text_img.resize(text_resize_amount)
-        resulting_img.paste(text_img, get_position_text(card_type, "Planet", "Text Box"), text_img)
-        if os.path.exists(cards_src):
-            cards_value_img = Image.open(cards_src, 'r').convert("RGBA")
-            cards_value_img = cards_value_img.resize((256, 176))
-            resulting_img.paste(cards_value_img, get_position_text(card_type, "Planet", "Card"), cards_value_img)
-        if os.path.exists(resources_src):
-            resources_value_img = Image.open(resources_src, 'r').convert("RGBA")
-            resources_value_img = resources_value_img.resize((202, 142))
-            resulting_img.paste(resources_value_img, get_position_text(card_type, "Planet", "Resource"),
-                                resources_value_img)
-        expansion_icon_img = Image.open(expansion_icon_src, 'r').convert("RGBA").resize((40, 40))
-        resulting_img.paste(expansion_icon_img, get_position_text(card_type, "Planet", "Expansion Icon"),
-                            expansion_icon_img)
-        add_name_to_card(card_type, name, resulting_img)
-        x_offset = int(690 - (0.5 * get_pil_text_size(
-            text, 84, "fonts/billboard-college-cufonfonts/Billboard-College.ttf"
-        )[2]))
-        add_text_to_planet_image(
-            resulting_img, text
-        )
-        num_icons = 0
-        for c in icons_grouped:
-            if c == "R":
-                material_src = "card_srcs/Planet/Icons/Material.png"
-                material_img = Image.open(material_src, 'r').convert("RGBA").resize((197, 278))
-                icon_coords = get_position_text(card_type, "Planet", "First Icon")
-                icon_coords = (icon_coords[0],
-                               icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
-                resulting_img.paste(material_img, icon_coords, material_img)
-                num_icons += 1
-            if c == "B":
-                technology_src = "card_srcs/Planet/Icons/Technology.png"
-                technology_img = Image.open(technology_src, 'r').convert("RGBA").resize((197, 278))
-                icon_coords = get_position_text(card_type, "Planet", "First Icon")
-                icon_coords = (icon_coords[0],
-                               icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
-                resulting_img.paste(technology_img, icon_coords, technology_img)
-                num_icons += 1
-            if c == "G":
-                strongpoint_src = "card_srcs/Planet/Icons/Strongpoint.png"
-                strongpoint_img = Image.open(strongpoint_src, 'r').convert("RGBA").resize((197, 278))
-                icon_coords = get_position_text(card_type, "Planet", "First Icon")
-                icon_coords = (icon_coords[0],
-                               icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
-                resulting_img.paste(strongpoint_img, icon_coords, strongpoint_img)
-                num_icons += 1
-            if c in ["R", "G", "B"] and num_icons > 1:
-                connector_src = "card_srcs/Planet/Icons/Icon_Join.jpg"
-                connector_img = Image.open(connector_src, 'r').convert("RGBA").resize((74, 45))
-                icon_coords = get_position_text(card_type, "Planet", "First Join")
-                print(icon_coords)
-                icon_coords = (icon_coords[0],  icon_coords[1] + (num_icons - 1)
-                               * get_position_text(card_type, "Planet", "Join Spacing"))
-                print(icon_coords)
-                resulting_img.paste(connector_img, icon_coords, connector_img)
-        resulting_img.save(output_dir, "PNG")
+        if process_submitted_planet_card(name, card_type, text, cards_value, resources_value, icons_grouped,
+                                         output_dir):
+            card_image = Image.open(output_dir)
+            card_image = card_image.resize((240, 342))
+            card_image = ImageTk.PhotoImage(card_image)
+            panel = tk.Label(master, image=card_image)
+            panel.place(x=650, y=150)
+            return True
+        return False
+    faction = faction_label.cget("text")
+    traits = traits_area.get("1.0", "end-1c")
+    faction = faction.replace("Faction: ", "")
+    cost = "0"
+    if card_type in ["Army", "Support", "Event", "Attachment"]:
+        cost = cost_area.get("1.0", "end-1c")
+    attack = "0"
+    health = "0"
+    if card_type in ["Army", "Warlord", "Synapse"]:
+        attack = attack_area.get("1.0", "end-1c")
+        health = health_area.get("1.0", "end-1c")
+    command = "0"
+    if card_type in ["Army", "Synapse"] and faction != "Neutral":
+        command = command_area.get("1.0", "end-1c")
+    loyalty = "Common"
+    if card_type == "Warlord":
+        loyalty = opt_loyalty.get()
+    starting_cards = "7"
+    starting_resources = "7"
+    if card_type == "Warlord":
+        starting_cards = starting_cards_area.get("1.0", "end-1c")
+        starting_resources = starting_resources_area.get("1.0", "end-1c")
+    shield_value = "0"
+    if card_type in ["Event", "Attachment"]:
+        shield_value = opt_shields.get()
+    if process_submitted_card(name, card_type, text, faction, traits, output_dir,
+                              attack=attack, health=health, command=command, cost=cost,
+                              starting_cards=starting_cards, starting_resources=starting_resources,
+                              loyalty=loyalty, shield_value=shield_value):
         card_image = Image.open(output_dir)
         card_image = card_image.resize((240, 342))
         card_image = ImageTk.PhotoImage(card_image)
         panel = tk.Label(master, image=card_image)
         panel.place(x=650, y=150)
+        return True
+    return False
+
+
+def process_submitted_planet_card(name, card_type, text, cards_value, resources_value, icons_grouped, output_dir):
+    resources_src = "card_srcs/Planet/Values/resource_" + resources_value + ".jpg"
+    cards_src = "card_srcs/Planet/Values/card_" + cards_value + ".jpg"
+    text_src = "card_srcs/" + card_type + "/Text/Text.png"
+    if not os.path.exists(text_src):
         return False
-    faction = faction_label.cget("text")
-    traits = traits_area.get("1.0", "end-1c")
-    faction = faction.replace("Faction: ", "")
+    card_art_src = "current_card_info/src_img/img.png"
+    expansion_icon_src = "current_card_info/expansion_icon/expansion_icon.png"
+    resulting_img = Image.new("RGBA", (1440, 2052))
+    card_art_img = Image.open(card_art_src, 'r').convert("RGBA")
+    card_art_img = card_art_img.resize((1440, 2052))
+    resulting_img.paste(card_art_img, get_position_text(card_type, "Planet", "Art"))
+    text_resize_amount = (1440, 2052)
+    text_img = Image.open(text_src, 'r').convert("RGBA")
+    text_img = text_img.resize(text_resize_amount)
+    resulting_img.paste(text_img, get_position_text(card_type, "Planet", "Text Box"), text_img)
+    if os.path.exists(cards_src):
+        cards_value_img = Image.open(cards_src, 'r').convert("RGBA")
+        cards_value_img = cards_value_img.resize((256, 176))
+        resulting_img.paste(cards_value_img, get_position_text(card_type, "Planet", "Card"), cards_value_img)
+    if os.path.exists(resources_src):
+        resources_value_img = Image.open(resources_src, 'r').convert("RGBA")
+        resources_value_img = resources_value_img.resize((202, 142))
+        resulting_img.paste(resources_value_img, get_position_text(card_type, "Planet", "Resource"),
+                            resources_value_img)
+    expansion_icon_img = Image.open(expansion_icon_src, 'r').convert("RGBA").resize((40, 40))
+    resulting_img.paste(expansion_icon_img, get_position_text(card_type, "Planet", "Expansion Icon"),
+                        expansion_icon_img)
+    add_name_to_card(card_type, name, resulting_img)
+    x_offset = int(690 - (0.5 * get_pil_text_size(
+        text, 84, "fonts/billboard-college-cufonfonts/Billboard-College.ttf"
+    )[2]))
+    add_text_to_planet_image(
+        resulting_img, text
+    )
+    num_icons = 0
+    for c in icons_grouped:
+        if c == "R":
+            material_src = "card_srcs/Planet/Icons/Material.png"
+            material_img = Image.open(material_src, 'r').convert("RGBA").resize((197, 278))
+            icon_coords = get_position_text(card_type, "Planet", "First Icon")
+            icon_coords = (icon_coords[0],
+                           icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
+            resulting_img.paste(material_img, icon_coords, material_img)
+            num_icons += 1
+        if c == "B":
+            technology_src = "card_srcs/Planet/Icons/Technology.png"
+            technology_img = Image.open(technology_src, 'r').convert("RGBA").resize((197, 278))
+            icon_coords = get_position_text(card_type, "Planet", "First Icon")
+            icon_coords = (icon_coords[0],
+                           icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
+            resulting_img.paste(technology_img, icon_coords, technology_img)
+            num_icons += 1
+        if c == "G":
+            strongpoint_src = "card_srcs/Planet/Icons/Strongpoint.png"
+            strongpoint_img = Image.open(strongpoint_src, 'r').convert("RGBA").resize((197, 278))
+            icon_coords = get_position_text(card_type, "Planet", "First Icon")
+            icon_coords = (icon_coords[0],
+                           icon_coords[1] + num_icons * get_position_text(card_type, "Planet", "Icon Spacing"))
+            resulting_img.paste(strongpoint_img, icon_coords, strongpoint_img)
+            num_icons += 1
+        if c in ["R", "G", "B"] and num_icons > 1:
+            connector_src = "card_srcs/Planet/Icons/Icon_Join.jpg"
+            connector_img = Image.open(connector_src, 'r').convert("RGBA").resize((74, 45))
+            icon_coords = get_position_text(card_type, "Planet", "First Join")
+            print(icon_coords)
+            icon_coords = (icon_coords[0], icon_coords[1] + (num_icons - 1)
+                           * get_position_text(card_type, "Planet", "Join Spacing"))
+            print(icon_coords)
+            resulting_img.paste(connector_img, icon_coords, connector_img)
+    resulting_img.save(output_dir, "PNG")
+    return True
+
+
+def process_submitted_card(name, card_type, text, faction, traits, output_dir,
+                           attack="0", health="0", command="0", cost="0",
+                           starting_cards="7", starting_resources="7",
+                           loyalty="Common", shield_value="0"):
+    global card_image
+    global panel
     text_src = "card_srcs/" + faction + "/" + card_type + "/Text.png"
     if not os.path.exists(text_src):
         return False
     card_art_src = "current_card_info/src_img/img.png"
     expansion_icon_src = "current_card_info/expansion_icon/expansion_icon.png"
-    output_dir = "current_card_info/resulting_image.png"
     first_command_src = "card_srcs/" + faction + "/" + card_type + "/First_Command.png"
     command_end_src = "card_srcs/" + faction + "/" + card_type + "/Command_End.png"
     extra_command_src = "card_srcs/" + faction + "/" + card_type + "/Extra_Command_Icon.png"
@@ -381,28 +428,22 @@ def process_submitted_card():
     add_text_to_image(resulting_img, text, get_position_text(card_type, faction, "Text"),
                       line_length=required_line_length)
     if card_type in ["Army", "Support", "Event", "Attachment"]:
-        cost = cost_area.get("1.0", "end-1c")
         add_text_to_image(
             resulting_img, cost, get_position_text(card_type, faction, "Cost"), font_size=168, color=(0, 0, 0)
         )
     if card_type in ["Army", "Warlord", "Synapse"]:
-        attack = attack_area.get("1.0", "end-1c")
-        health = health_area.get("1.0", "end-1c")
         add_text_to_image(
             resulting_img, attack, get_position_text(card_type, faction, "Attack"), font_size=168, color=(255, 255, 255)
         )
         add_text_to_image(
             resulting_img, health, get_position_text(card_type, faction, "Health"), font_size=168, color=(0, 0, 0)
         )
-    if card_type == "Army" and faction != "Neutral":
-        command = command_area.get("1.0", "end-1c")
+    if card_type in ["Army", "Synapse"] and faction != "Neutral":
         try:
             add_command_icons(command, first_command_src, extra_command_src, command_end_src, resulting_img, faction)
         except ValueError:
             pass
     if card_type == "Warlord":
-        starting_cards = starting_cards_area.get("1.0", "end-1c")
-        starting_resources = starting_resources_area.get("1.0", "end-1c")
         add_text_to_image(
             resulting_img, starting_cards, get_position_text(card_type, faction, "Cards"),
             font_size=168, color=(0, 0, 0)
@@ -412,7 +453,6 @@ def process_submitted_card():
             font_size=168, color=(243, 139, 18)
         )
     if card_type in ["Army", "Support", "Event", "Attachment"]:
-        loyalty = opt_loyalty.get()
         if (loyalty == "Loyal" or loyalty == "Signature") and faction != "Neutral":
             loyalty_src = "card_srcs/" + faction + "/Loyalty/" + loyalty + ".png"
             loyalty_img = Image.open(loyalty_src, 'r').convert("RGBA")
@@ -422,7 +462,6 @@ def process_submitted_card():
             loyalty_img = loyalty_img.resize(resize_loyalty)
             resulting_img.paste(loyalty_img, get_position_loyalty(faction, card_type), loyalty_img)
     if card_type in ["Event", "Attachment"]:
-        shield_value = opt_shields.get()
         shield_value = int(shield_value)
         if shield_value > 0:
             shield_src = "card_srcs/" + faction + "/Shield/Shield_Icon.png"
@@ -433,11 +472,6 @@ def process_submitted_card():
                 resulting_img.paste(shield_icon_img, starting_position_shield, shield_icon_img)
                 starting_position_shield = (starting_position_shield[0], starting_position_shield[1] + 100)
     resulting_img.save(output_dir, "PNG")
-    card_image = Image.open(output_dir)
-    card_image = card_image.resize((240, 342))
-    card_image = ImageTk.PhotoImage(card_image)
-    panel = tk.Label(master, image=card_image)
-    panel.place(x=800, y=150)
     return True
 
 
@@ -591,7 +625,7 @@ command_area = tk.Text(master, height=1, width=2)
 starting_amounts_label = tk.Label(master, text="Starting amounts (Cards/Resources):", font=("Arial", 12))
 starting_cards_area = tk.Text(master, height=1, width=2)
 starting_resources_area = tk.Text(master, height=1, width=2)
-submit_card = tk.Button(master, text="Submit Card", command=process_submitted_card, font=("Arial", 12),
+submit_card = tk.Button(master, text="Submit Card", command=get_parameters_then_process, font=("Arial", 12),
                         background="green")
 
 planet_resources_label = tk.Label(master, text="Resources:", font=("Arial", 12))
